cdf11574146d175e682606efc12a60e6247112c5
jawadUlHassan069 <jawadhassn0204@gmail.com>
2026-06-27 23:47:27 +0500

    render db configure update

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -88,6 +88,9 @@ DATABASES = {
         'PASSWORD': config('DB_PASSWORD', default=''),
         'HOST':     config('DB_HOST',     default='localhost'),
         'PORT':     config('DB_PORT',     default='5432'),
+        'OPTIONS': {
+            'sslmode': 'require',
+        },
     }
 }
 
